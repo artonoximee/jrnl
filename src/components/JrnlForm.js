9330dbacd73cc213a000cc52901dbdf1b76949ab
Arton Oximée (Antoine Vercoutère) <artonoximee@proton.me>
2022-11-10 15:24:13 +0100

    layout is set and firebase is configured

--- src/components/JrnlForm.js
+++ src/components/JrnlForm.js
@@ -7,7 +7,27 @@ function JrnlForm() {
 
   return(
     <>
-    <input />
+    <div className="row align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+      <div className="col-lg-6 col-md-10">
+        <form>
+          <input 
+            type="text" 
+            className="form-control form-control-lg text-light border-secondary"
+            placeholder="identifier"
+          />
+
+          <textarea
+            type="text"
+            rows="5"
+            placeholder="note"
+            className="mt-5 form-control form-control-lg text-light border-secondary"
+          />
+
+          <button className="btn btn-lg btn-outline-secondary mt-5 w-100">jrnl</button>
+        </form>
+      </div>
+    </div>
+    
     </>
   )
 }
